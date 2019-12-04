94e18c404d54ebbb23099e183e3c047bc04d5466
Dane Smith <doctorhowser@gmail.com>
2019-12-04 14:42:07 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# JS Practice: Loops and Arrays
+
+## Objective
+
+Practice translating written instructions into JavaScript code. Focuses on variables, loops, arrays, and functions.
+
+## Instructions
+
+Inside the `client.js` are a set of steps written out in long form. Each numbered step needs to be implemented in JavaScript. Write your code beneath each step and turn in your completed code repo.
+
+**Be sure to test regularly and use plenty of `console.logs()` to help find your errors!**
---
+++ client.js
@@ -0,0 +1,28 @@
+/*
+1. Start with the number 42 and set that equal to a variable named 'myNumber'
+
+2. Create condition logic to check if the myNumber is greater or equal to 53
+2-1. If true, add 42 to 'myNumber'
+2-2. If false, subtract 13 from 'myNumber'
+
+3. Create a string that is set to 11, add it to 'myNumber'
+
+4. Create an array called 'numberArray'. Loop through 'myNumber' (now a string!), and using charAt(), set numberArray[i] to each value from 'numberArray'
+
+5. Remove the first and last values from 'numberArray'
+
+6. Create a new variable. Loop **backwards** through 'numberArray' and concatenate each value from the array into the new variable (backwards, remember!)
+
+7. parseInt() both the 'myNumber' and the new variable created in Step 6, ensure that both 'myNumber' and the new variable are set to these new parsed values
+
+8. Add 'myNumber' and the new variable created in Step 6 together and store them in 'myNumber'
+
+9. If the new value of 'myNumber' is less than 60, set 'myNumber' equal to 14. If not, check to see if it is equal to 2930, if it is, set 'myNumber' equal to 27. If neither of these are true, set the value to 2.
+
+10. Create a while loop that runs 10 times and increments 'myNumber' by 1 each time through the loop.
+
+11. Create a function that accepts an argument 'val'. The function should convert 'val' to a String, then drop the first character off the String, but only if there
+is more than 1 character in the String. Return 'val' and set the variable 'myNumber' to the returned value.
+
+12. Your answer should be a String value that equals 7. Is that what you got?
+*/
