97a878dc86e30dfa6125c3db55c8c7fd8fb13a3b
Kris Szafranski <kris@thoughtshelter.com>
2022-05-03 15:25:10 -0500

    removed jquery, stubbed in function signature, updated instrructions

--- client.js
+++ client.js
@@ -1,48 +1,47 @@
 /*
-You’ve been given an index.html file and a starter js file, and jQuery.
+You’ve been given an index.html file and this starter js file.
 
-0. Source this file and the jquery.js files into the index.html file. You should see a console log when this is done correctly.
+0. Source this file into the index.html file. You should see a console log when this is done correctly.
   -- There is no JS to write for this step.
 */
-$(document).ready(function() {
-  console.log('Step 0 complete! jquery and client.js loaded!');
-              
-  // Write your code here!
 
-  // 1. Start with the number 29 and set that equal to a variable named ‘highScore’;
-  
-  // 2. Make a new empty array called myScrabbleTiles.
-  
-  // 3. Put these 6 tile objects into myScrabbleTiles:
-  /*
-    { tile: 'N', score: 1 }
-    { tile: 'K', score: 5 }
-    { tile: 'Z', score: 10 }
-    { tile: 'X', score: 8 }
-    { tile: 'D', score: 2 }
-    { tile: 'A', score: 1 }
-  */
-  
-  // 4. Remove the last tile from myScrabbleTiles.
-  
-  // 5. Add the following new tile to myScrabbleTiles:
-  /*
-    { tile: 'F', score : 4 }
-  */
-  
-  // 6. Write a function that takes in an array. The function will use a for-loop and returns the sum of all the scores in the given array.
-  
-  // 7. Use the function above to get the total score for myScrabbleTiles and assign the value to a variable called "myScore".
-  
-  // 8. Check whether or not your score is higher than the highScore.
-    // 8-1 If your score is higher, change highScore to the new high score.
-  
-  
-// STRETCH GOALS!!! 
-  
-  // 9. Using jQuery, Append to the DOM the highScore value.
-  
-  // 10. Using jQuery, Append to the DOM myScrabbleTiles as an unordered list
+console.log('Step 0 complete! client.js loaded!');
+            
+// Write your code here!
 
-});
-// DO NOT WRITE CODE BEYOND HERE 
+// 1. Start with the number 29 and set that equal to a variable named ‘highScore’;
+
+// 2. Make a new empty array called myScrabbleTiles.
+
+// 3. Put these 6 tile objects into myScrabbleTiles:
+/*
+  { tile: 'N', score: 1 }
+  { tile: 'K', score: 5 }
+  { tile: 'Z', score: 10 }
+  { tile: 'X', score: 8 }
+  { tile: 'D', score: 2 }
+  { tile: 'A', score: 1 }
+*/
+
+// 4. Remove the last tile from myScrabbleTiles.
+
+
+// 5. Add the following new tile to myScrabbleTiles:
+/*
+  { tile: 'F', score : 4 }
+*/
+
+// 6. Complete this function. It needs to be given an array of tile objects. The function will use a for-loop and return the sum of all the scores in the given array.
+function sumTiles(anArray) {
+
+}
+
+
+// 7. Use the function above to get the total score for myScrabbleTiles and assign the value to a variable called "myScore".
+
+
+// 8. Check whether or not your score is higher than the highScore.
+// 8-1 If your score is higher, change highScore to the new high score.
+
+
+// DONE!
